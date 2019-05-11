a8392b8475980408b01fc1bc7b1fa4e681efc08c
tttedu304 <eduherere@hotmail.com>
2019-05-11 08:08:13 -0500

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Searchbar from "./components/searchbar";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+function App() {
+  return (
+    <div className="App">
+      <Searchbar />
+      <p>YEA BOY!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/searchbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+import "./scss/searchbar.scss";
+
+class SearchBar extends Component {
+  state = { open: false };
+
+  handleClick = () => {
+    this.setState(state => ({ open: !state.open }));
+  };
+  render() {
+      // eslint-disable-next-line no-restricted-globals
+    const searchClass = classNames({ open, search: true });
+
+    return (
+      <div className={this.searchClass}>
+        <input type="search" className="search-box" />
+        <span className="search-button" onClick={this.handleClick}>
+          <span className="search-icon" />
+        </span>
+      </div>
+    );
+  }
+}
+export default SearchBar;
