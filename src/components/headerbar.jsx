687d4b5ad16bcaae092a01d1b35619f6fbfc0b63
tttedu304 <eduherere@hotmail.com>
2019-05-20 18:57:10 -0500

    for remake

--- src/components/headerbar.jsx
+++ src/components/headerbar.jsx
@@ -1,20 +1,78 @@
 import React, { Component } from "react";
-import classNames from "classnames";
-import "./scss/headerbar.scss";
-import logo from "./assets/img/logo.png";
+//import "./scss/headerbar.scss";
+//import logo from "./assets/img/logo.png";
+import "materialize-css/dist/css/materialize.min.css";
+//<img src={logo} alt="Logo" className="header__logo--img" />
 
 class HeaderBar extends Component {
   render() {
     return (
-      <header className="header">
-        <figure className="header__logo">
-          <img src={logo} alt="Logo" className="header__logo--img" />
-        </figure>
-        <nav className="header__nav">
-          {/* eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/anchor-is-valid */}
-          <a href="#" className="header__nav--menu" />
+      <React.Fragment>
+        <nav className="nav-extended">
+          <div className="nav-wrapper">
+            <a href="/" className="brand-logo">
+              Logo
+            </a>
+            <a href="/" data-target="mobile-demo" className="sidenav-trigger">
+              <i className="material-icons">menu</i>
+            </a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <a href="sass.html">Sass</a>
+              </li>
+              <li>
+                <a href="badges.html">Components</a>
+              </li>
+              <li>
+                <a href="collapsible.html">JavaScript</a>
+              </li>
+            </ul>
+          </div>
+          <div className="nav-content">
+            <ul className="tabs tabs-transparent">
+              <li className="tab">
+                <a href="#test1">Test 1</a>
+              </li>
+              <li className="tab">
+                <a className="active" href="#test2">
+                  Test 2
+                </a>
+              </li>
+              <li className="tab disabled">
+                <a href="#test3">Disabled Tab</a>
+              </li>
+              <li className="tab">
+                <a href="#test4">Test 4</a>
+              </li>
+            </ul>
+          </div>
         </nav>
-      </header>
+
+        <ul className="sidenav" id="mobile-demo">
+          <li>
+            <a href="sass.html">Sass</a>
+          </li>
+          <li>
+            <a href="badges.html">Components</a>
+          </li>
+          <li>
+            <a href="collapsible.html">JavaScript</a>
+          </li>
+        </ul>
+
+        <div id="test1" className="col s12">
+          Test 1
+        </div>
+        <div id="test2" className="col s12">
+          Test 2
+        </div>
+        <div id="test3" className="col s12">
+          Test 3
+        </div>
+        <div id="test4" className="col s12">
+          Test 4
+        </div>
+      </React.Fragment>
     );
   }
 }
